3eba4316c61fb3aaf0e32ba30c4b62bb1b2dce6a
Ben Biddington <ben.biddington@gmail.com>
2010-02-22 13:52:55 +0000

    Ignoring .idea

---
+++ test/integration.tests/jotify_spec.rb
@@ -0,0 +1,24 @@
+require 'spec'
+require 'jotify'
+
+describe "The Jotify class" do
+	before(:all) do
+		@jotify = Jotify.new
+	end
+
+	after(:all) do
+		@jotify.close rescue nil
+	end
+
+	it "should allow me to read my playlists from Spotify" do
+		playlists = @jotify.playlists
+
+		playlists.length.should be > 0
+	end
+
+	it "should allow to search Spotify" do
+		search_results = @jotify.search('Chubby')
+
+		search_results.artists.length.should be > 0
+	end
+end
