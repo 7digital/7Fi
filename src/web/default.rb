db8cc9145f0942d713e35908bf677d092740b2f3
Ben Biddington <ben.biddington@gmail.com>
2010-02-23 13:39:54 +0000

    Can't get variables to bind in haml template

--- src/web/default.rb
+++ src/web/default.rb
@@ -1,8 +1,15 @@
 require 'rubygems'
+require 'haml'
 require 'sinatra'
+require File.dirname(__FILE__) + '/models/home_model'
 
 class Default
-	  get '/' do
-		  'Home screen'
-	  end
+	set :views          => File.dirname(__FILE__) + '/views',
+	    :dump_errors    => true,
+		:raise_errors   => true,
+		:port           => 82
+
+	get '/default.html' do
+		haml :home, :locals => { :title => "xxx" }
+	end
 end
---
+++ src/web/models/home_model.rb
@@ -0,0 +1,7 @@
+class HomeModel
+	attr_accessor :title
+
+	def initialize
+
+	end
+end
