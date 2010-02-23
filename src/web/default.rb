aacb00f41766b37d23df1cde0d899f3b2a92412c
Ben Biddington <ben.biddington@gmail.com>
2010-02-23 17:54:23 +0000

    Added some UI elements

--- src/web/default.rb
+++ src/web/default.rb
@@ -1,37 +1,44 @@
 require 'rubygems'
 require 'haml'
 require 'sinatra'
+require 'sinatra/base'
 require 'jotify'
 require 'json'
 require File.dirname(__FILE__) + '/../../src/seven_digital/api'
 require File.dirname(__FILE__) + '/models/home_model'
 
+# TODO: Consider this example: http://github.com/ben-biddington/spotify-api/blob/master/lib/jotify/api.rb
 class Default
 	set :views          => File.dirname(__FILE__) + '/views',
 	    :dump_errors    => true,
 		:raise_errors   => true,
 		:port           => 82
 
-	#set :public, "/web/public"
-	
 	get '/default.html' do
-		model = HomeModel.new("Home")
+		model = HomeModel.new("7Fi -- Home", 'ben.biddington')
 
 		haml :home, :locals => { :model => model }
 	end
 
 	get '/playlists.json' do
 		response["Cache-Control"] = "max-age=3600, public"
-		
-		content_type :json
-		{
-			'status' => 'OK',
-			'result' => {
-				'playlists' =>
-				Jotify.new.playlists.map do |p|
-					p.to_h
-			    end.each { |h| h['size'] = h.delete(:tracks).size }
-			}
-		}.to_json
+		response["Expires"] = "3600"
+
+		jotify = Jotify.new
+
+		begin
+			content_type :json
+			{
+				'status' => 'OK',
+				'result' => {
+					'playlists' =>
+					jotify.playlists.map do |p|
+						p.to_h
+					end.each { |h| h['size'] = h.delete(:tracks).size }
+				}
+			}.to_json
+		ensure
+			jotify.close unless jotify.nil?	
+		end
 	end
 end
--- src/web/models/home_model.rb
+++ src/web/models/home_model.rb
@@ -1,7 +1,8 @@
 class HomeModel
-	attr_accessor :title
+	attr_accessor :title, :spotify_username
 
-	def initialize(title)
+	def initialize(title, spotify_username)
 		@title = title
+		@spotify_username = spotify_username
 	end
 end
