6ba93343345e54c036d85e8170bb98442c0268cc
Ben Biddington <ben.biddington@gmail.com>
2010-03-05 09:21:10 +0000

    Describing last.fm artist image search

--- test/integration.tests/artwork/last.fm/last_fm_search_spec.rb
+++ test/integration.tests/artwork/last.fm/last_fm_search_spec.rb
@@ -15,6 +15,13 @@ describe "Searching Last.fm for artist photos" do
 		@result.should include('<images artist="Chubby Jackson"')
 	end
 
+	it "should return a list of artist images" do
+		pending("Not yet implemented, but the searcher should translate the API response to a list of objects") do
+			when_searching_for('Chubby Jackson')
+			@result.should be_a List
+		end
+	end
+
 	def when_searching_for(what)
 		@result = @lastfm.find(what)
 	end
--- test/integration.tests/jotify_spec.rb
+++ test/integration.tests/jotify_spec.rb
@@ -10,11 +10,13 @@ describe "The Jotify class" do
 		@jotify.close rescue nil
 	end
 
-	#	it "should allow me to read my playlists from Spotify" do
-	#		playlists = @jotify.playlists
-	#
-	#		playlists.length.should be > 0
-	#	end
+	it "should allow me to read my playlists from Spotify" do
+		pending ('The underlying API is flaky, test is slow and likely we will remove Spotify anyway')
+
+		playlists = @jotify.playlists
+
+		playlists.length.should be > 0
+	end
 
 	it "should allow me to search Spotify" do
 		search_results = @jotify.search('Chubby')
