c4571189ff2ce2a5bf2f8c21792e1b1b26d25171
Ben Biddington <ben.biddington@gmail.com>
2010-03-05 08:51:27 +0000

    Describing last.fm artist image search

---
+++ src/seven_digital/artwork/last_fm_search.rb
@@ -0,0 +1,30 @@
+require 'cgi'
+require File.dirname(__FILE__) + '/../../../src/seven_digital/net/the_internet'
+
+class LastFmSearch
+	import 'java.net.URI'
+
+	def initialize(api_key)
+		@api_key = api_key
+	end
+
+	# See: http://www.last.fm/api/show?service=407
+	def find(artist_name)
+		  the_url =
+			"#{API_URL}?method=artist.getimages&artist&api_key=#{@flickr_api_key}&" +
+			"artist=#{uri_encode(artist_name)}&"+
+			"api_key=#{@api_key}"
+
+		puts the_url
+		  
+		TheInternet.new.get(URI.new(the_url))
+	end
+
+	private
+	
+	API_URL = "http://ws.audioscrobbler.com/2.0/"
+
+	def uri_encode(what)
+		CGI::escape(what)
+	end
+end
---
+++ test/integration.tests/artwork/last_fm_search_spec.rb
@@ -0,0 +1,25 @@
+require "spec"
+require File.dirname(__FILE__) + '/../../../src/seven_digital/artwork/last_fm_search'
+
+describe "Searching Last.fm for photos" do
+	@result = nil
+	@lastfm = nil
+
+	before(:all) do
+		@result = nil
+		@lastfm = LastFmSearch.new('ff189f0167fd99d906cab4dfcbf89f48')
+	end
+	
+	it "should return a list of photos as text" do
+		when_searching_for('Chubby Jackson')
+		@result.should include('<images artist="Chubby Jackson"')
+	end
+
+	def when_searching_for(what)
+		@result = @lastfm.find(what)
+	end
+
+	def then_result_is_ok
+		@result.should include('<lfm status="ok">') 
+	end
+end
