c5b4f4f051e34f904479ee8fac88c51d62722f30
Ben Biddington <ben.biddington@gmail.com>
2010-03-25 16:59:55 +0000

    Example: querying a store

--- src/seven_fi/apps/seven_fi/models/artist.js
+++ src/seven_fi/apps/seven_fi/models/artist.js
@@ -2,6 +2,6 @@ SevenFi.Artist = SC.Record.extend(
 /** @scope SevenFi.Artist.prototype */ {
 	primaryKey 	: "id",
 	name 		: SC.Record.attr(String),
-	id 			: SC.Record.attr(String),
+	id 			: SC.Record.attr(Number),
 	picture_url	: SC.Record.attr(String)
 });
--- src/seven_fi/apps/seven_fi/tests/controllers/how.to.js
+++ src/seven_fi/apps/seven_fi/tests/controllers/how.to.js
@@ -2,61 +2,67 @@
 // http://localhost:4020/seven_fi/en/current/tests/controllers/how.to.html
 
 sc_require('models/task');
-SevenFi.ARTIST_SEARCH_QUERY = SC.Query.remote(
-	SevenFi.Artist,
-	{ orderBy: 'name'}
-);
-
 module("SevenFi.SearchDataSource");
 
 // @see: http://wiki.sproutcore.com/Foundation-Ajax+Requests
 // @see: http://wiki.sproutcore.com/DataStore-Your+First+Find
-test("How to do an artist search", function() {
-	var theUrl = '/7digital/search/artists?q=chubby+jackson';
-
-	var store = SC.Store.create({
-		commitRecordsAutomatically: YES
-	}).from('SevenFi.SearchDataSource');
-
-	console.log("Starting...");
+test("How to load json into a store of SevenFi.Artist objects", function() {
+	var theUrl = '/7digital/search/artists?q=chubby+rain';
 
 	var callback = function(response, store, query) {
 		if (SC.ok(response)) {
 			console.debug("Loading store...");
+			
+			var storeKeys = store.loadRecords(
+				SevenFi.Artist,
+				response.getPath('body').results.results
+			);
 
-			try {
-				var storeKeys = store.loadRecords(
-					SevenFi.Artist,
-					response.getPath('body').results.results
-				);
+			console.debug("Loaded <%@1> store keys".fmt(storeKeys.get('length')));
 
-				store.loadQueryResults(query, storeKeys);
-
-				var allArtists = store.find(SevenFi.Artist);
-
-				console.debug("Response status was <%@1>".fmt(response.getPath('body').status));
-				console.debug("There were supposed to be <%@1> artists fetched".fmt(response.getPath('body').results.count));
-				console.debug("There were <%@1> artists found".fmt(allArtists.get('length')));
-				console.debug("There were <%@1> artists found".fmt(allArtists.toArray().get('length')));
-				console.debug("There first one is <%@1>".fmt(allArtists.objectAt(0)));
-
-				var q = SC.Query.local(SevenFi.Artist, "name = %@", "chubby");
-				var chubbyJacksonResults = store.find(q);
-
-				console.debug("Result for chubby jackson <%@1>".fmt(chubbyJacksonResults.objectAt(0)));
-
-			} catch (e) {
-				console.debug(e.toString());	
-			}
-
+			equals(50, storeKeys.get('length'), "Expected 50 results to be returned.");
 		} else {
 			console.debug("ERROR");
 		}
 	};
 
-	var result = SC.Request.getUrl(theUrl).json().
-		notify(this, callback, store, null).
-		send();
+	var dummyStore = SC.Store.create({});
 
-	console.log(result);
+	var result = SC.Request.getUrl(theUrl).json().
+		notify(this, callback, dummyStore, null).
+		send();
+});
+
+test("How to query a store", function() {
+	var store = SC.Store.create({});
+
+	var exampleJson = JSON.parse(
+		'{"status":"OK","results":{"results":[' +
+		'	{"name":"Popa Chubby","id":"54520","picture_url":"http://xxx.jpg"},' +
+		'	{"name":"Chubby Bat","id":"54521","picture_url":"http://xxx.jpg"}' +
+		']}}'
+	);
+
+	var keys = store.loadRecords(
+		SevenFi.Artist,
+		exampleJson.results.results
+	);
+
+	console.debug("Store contains <%@1> items".fmt(keys.length));
+
+	var query = SC.Query.local(
+		SevenFi.Artist,
+		"name = {name}",
+		{ name : 'Popa Chubby'},
+		{ orderBy: 'name'}
+	);
+
+	var results = store.find(query);
+
+	var actualResultCount = results.length();
+
+	var theSearchResult = results.objectAt(0);
+
+	equals(1, actualResultCount);
+	equals(theSearchResult.get('id'), 54520);
 });
