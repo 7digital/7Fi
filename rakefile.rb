e3561c169d9f9c8ab3d21fcbeb435b451fb33418
Ben Biddington <ben.biddington@gmail.com>
2010-03-28 15:29:02 +0100

    Windows help

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ desc 'Start web API'
 task :api do |t|
 	bootstrap = "#{File.dirname(__FILE__)}/src/web/default.rb"
 
-	#bootstrap.replace(bootstrap.gsub(/\//, '\\')) if is_windows?	
+	bootstrap.replace(bootstrap.gsub(/\//, '\\')) if is_windows?	
 
 	system("cmd", "/c", "jruby", "-S", "#{bootstrap}", "/K")
 end
