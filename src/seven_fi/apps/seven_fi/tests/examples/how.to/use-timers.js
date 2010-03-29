42466e1e0981895f04c6daedb66d1e5b46d6f844
Ben Biddington <ben.biddington@gmail.com>
2010-03-29 17:33:58 +0100

    More on Timer

--- src/seven_fi/apps/seven_fi/tests/examples/how.to/use-timers.js
+++ src/seven_fi/apps/seven_fi/tests/examples/how.to/use-timers.js
@@ -1,16 +1,30 @@
 // Run this fixture by browsing to:
 // http://localhost:4020/seven_fi/en/current/tests/examples/how.to/use-timers.html
 
+var timer;
+
 // See: http://docs.sproutcore.com/symbols/SC.Timer.html
+module("sevenFi.examples.how.to", {
+	setup 	: function() {},
+	teardown: function() {
+		if (timer != null) {
+			timer.invalidate();
+			timer.destroy();
+			timer = null;
+		}
+	}
+});
 
 test("You can supply a function", function() {
 	var timeoutInMilliseconds = 500;
 	
-	SC.Timer.schedule({
+	timer = SC.Timer.schedule({
 		action : function() {
 			ok(YES, "Timer executed after %@1ms".fmt(timeoutInMilliseconds));
 
 			start();
+
+			timer.invalidate();
 		},
 		interval : timeoutInMilliseconds
 	});
@@ -24,16 +38,22 @@ test("Or you can supply a target", function() {
 			ok(YES, "Timer executed after %@1ms".fmt(timeoutInMilliseconds));
 
 			start();
+
+			timer.invalidate();
 		}
 	});
 
 	var timeoutInMilliseconds = 500;
 
-	SC.Timer.schedule({
+	timer = SC.Timer.schedule({
 		target : anyObject,
 		action 	: 'onTimer',
 		interval : timeoutInMilliseconds
 	});
 
 	stop();
+});
+
+test("Those two timers seem to interfere, why?", function() {
+	ok(YES, "@pending");
 });
